543f3d4f32730cc23f99802c8fbac4b3c551ce07
Braydon Jones <bradofrado@gmail.com>
2022-01-26 17:35:11 -0700

    Made a routing configuration

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-var lastPage = "home";
-function onNavClick(e)
-{
-  const id = e.id ? e.id : e;
-  const pageToLoad = `/pages/${id}.html`;
+const myApp = "#app";
 
-  $("#content").load(pageToLoad);
-  scroll(0,0);
+const routers = {};
+const views = {};
 
-  $(`#${lastPage}`).removeClass("active");
-  $(`#${id}`).addClass("active");
+var view = function(name, id, templateFunction, path) {
+    views[name] = {content: templateFunction, id: id};
 
-  lastPage = id;
+    if (path) {
+        route(path, templateFunction);
+    }
 }
 
-function onLoad()
-{
-  onNavClick({id: "home"});
-}     
+var route = function(path, template, id) {
+    if (typeof template === "function")
+    {
+        routers[path] = {content: template, id: id};
+    }
+    else if (typeof template === "string")
+    {
+        routers[path] = {content: views[template].content, id: id};
+    }
+}
+
+let router = function() {
+    const path = window.location.hash.slice(1) || "/";
+
+    const view = routers[path];
+    const id = view.id ? view.id : myApp;
+
+    $(id).html(view.content());
+}
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
+
+route('/', function() {
+    return "<a href='/home'>Hello, World</div>";
+})
