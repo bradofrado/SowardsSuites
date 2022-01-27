b8479dc213aec333c734a55ffc92f87b459ff4bd
Braydon Jones <bradofrado@gmail.com>
2022-01-26 20:37:56 -0700

    Split up the routing into a new file

---
+++ S.Routing.js
@@ -0,0 +1,71 @@
+S.Routing = (function () {
+    const routers = {};
+    const views = {};
+    
+    this.view = function(name, id, templateFunction, path) {
+        views[name] = {content: templateFunction, id: id};
+    
+        if (path) {
+            route(path, templateFunction);
+        }
+    }
+    
+    this.route = function(path, template, id) {
+        if (typeof template === "function")
+        {
+            routers[path] = {content: template, id: id};
+        }
+        else if (typeof template === "string")
+        {
+            routers[path] = {content: views[template].content, id: id};
+        }
+    }
+    
+    this.router = function() {
+        const path = window.location.hash.slice(1) || "/";
+    
+        let view = routers[path];
+    
+        if (view === undefined) {
+            view = views['404'];
+        }
+    
+        const id = view.id ? view.id : S.app;
+    
+        $(id).html(view.content());
+    }
+
+    this.view('404', null, function () {
+        return `<div>
+                    <h1>404 File Not Found</h1>
+                    <p>Sorry, the file you are looking for does not exist</p>
+                </div`
+    })
+
+    this.view('home', null, function () {
+        return "<h1>Home</h1>";
+    })
+    
+    this.route('/', 'home');
+    
+    this.route('/index.html', 'home');
+
+    this.route('/home', 'home');
+
+    window.addEventListener('load', this.router);
+    window.addEventListener('hashchange', this.router);    
+});
+
+var r = new S.Routing();
+
+S.Routing.view = function(name, id, templateFunction, path) {
+    r.view(name, id, templateFunction, path);
+}
+
+S.Routing.path = function(path, template, id) {
+    r.path(path, template, id);
+}
+
+S.Routing.route = function () {
+    r.router();
+}
--- main.js
+++ main.js
@@ -1,79 +1,48 @@
-const myApp = "#app";
-
-const routers = {};
-const views = {};
-
-var view = function(name, id, templateFunction, path) {
-    views[name] = {content: templateFunction, id: id};
-
-    if (path) {
-        route(path, templateFunction);
-    }
-}
-
-var route = function(path, template, id) {
-    if (typeof template === "function")
-    {
-        routers[path] = {content: template, id: id};
-    }
-    else if (typeof template === "string")
-    {
-        routers[path] = {content: views[template].content, id: id};
-    }
-}
-
-let router = function() {
-    const path = window.location.hash.slice(1) || "/";
-
-    let view = routers[path];
-
-    if (view === undefined) {
-        view = views['404'];
+(function () {
+    S = {
+        debug: false,
+        app: "#app"
     }
 
-    const id = view.id ? view.id : myApp;
-
-    $(id).html(view.content());
-}
-
-let onLoad = function () {
-    fixHref("body");
-    router();
-}
-
-window.addEventListener('load', onLoad);
-window.addEventListener('hashchange', router);
-
-view('404', null, function () {
-    return `<div>
-                <h1>404 File Not Found</h1>
-                <p>Sorry, the file you are looking for does not exist</p>
-            </div`
-})
-
-route('/', function() {
-    return "<a href='/home'>Hello, World</div>";
-})
-
-route('/portfolio', function () {
-    return "<div>I am in the Portfolio</div><a href='/'>Return Home</a>"
-})
-
-var fixHref = function(target) {
-    const atags = $(target).find('a');
-
-    for (let i = 0; i < atags.length; i++)
-    {
-        const oldHref = $(atags[i]).attr('href');
-        $(atags[i]).attr('href', `#${oldHref}`);
+    let onLoad = function () {
+        S.Fix.href("body");        
     }
-}
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(function (mutationsList, observer) {
-    fixHref(mutationsList[0].target);
-});
-const config = {attributes: true, childList: true, subtree: true};
+    window.addEventListener('load', onLoad);
+    
+    S.Fix = (function(){
+        var _fix = function (target) {
+            const _target = target ? target : "app";
 
-// Start observing the target node for configured mutations
-observer.observe(document.getElementById('app'), config);
+            // Create an observer instance linked to the callback function
+            const observer = new MutationObserver(function (mutationsList, observer) {
+                fixHref(_target);
+            });
+            const config = {attributes: true, childList: true, subtree: true};
+            
+            // Start observing the target node for configured mutations
+            observer.observe(document.getElementById(_target), config);  
+        }
+
+        _fix.fixHref = function(target) {
+            const atags = $(target).find('a');
+        
+            for (let i = 0; i < atags.length; i++)
+            {
+                const oldHref = $(atags[i]).attr('href');
+                $(atags[i]).attr('href', `#${oldHref}`);
+            }
+        }
+        
+        return _fix;
+    });
+
+    let fix = new S.Fix();
+
+    S.Fix.href = function (target) {
+        fix.fixHref(target);
+    }
+    
+      
+})();
+
