1d36e15640e13a9d4db77c0cdb47d821e835de68
Braydon Jones <bradofrado@gmail.com>
2022-01-26 17:48:35 -0700

    Added functions to automatically fix the routes to be #/...

--- main.js
+++ main.js
@@ -26,14 +26,43 @@ let router = function() {
     const path = window.location.hash.slice(1) || "/";
 
     const view = routers[path];
+
+    if (view === undefined) {
+        return;
+    }
+
     const id = view.id ? view.id : myApp;
 
     $(id).html(view.content());
 }
 
-window.addEventListener('load', router);
+let onLoad = function () {
+    fixHref("body");
+    router();
+}
+
+window.addEventListener('load', onLoad);
 window.addEventListener('hashchange', router);
 
 route('/', function() {
     return "<a href='/home'>Hello, World</div>";
 })
+
+var fixHref = function(target) {
+    const atags = $(target).find('a');
+
+    for (let i = 0; i < atags.length; i++)
+    {
+        const oldHref = $(atags[i]).attr('href');
+        $(atags[i]).attr('href', `#${oldHref}`);
+    }
+}
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(function (mutationsList, observer) {
+    fixHref(mutationsList[0].target);
+});
+const config = {attributes: true, childList: true, subtree: true};
+
+// Start observing the target node for configured mutations
+observer.observe(document.getElementById('app'), config);
