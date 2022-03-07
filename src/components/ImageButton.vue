8ddc024cd4264cc0f642ee7016029d76da35121f
Braydon Jones <bradofrado@gmail.com>
2022-03-07 10:26:16 -0700

    Made Home view

---
+++ src/components/ImageButton.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="image-button-container">
+        <router-link name="image-button" :class="'image-button center-center' + (img ? ' no-color' : '')" :to="to">            
+            <div name="image-button-text" class="image-button-text center-center" >{{name}}</div>
+        </router-link>
+        <img v-if="img" :src="`/images/${img}`">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ImageButton",
+    props: {
+        to: String,
+        name: String,
+        img: String
+    }
+}
+</script>
+
+<style scoped>
+img {
+    width: 100%;
+}
+
+.image-button-container {
+    width: 200px;
+    height: 200px;
+    margin: 20px auto;
+    position: relative;
+}
+
+.image-button {
+    position: relative;
+    color: inherit;
+    background-color: #999;
+    transition: background-color .3s ease-in-out;
+    width: 200px;
+    height: 200px;
+}
+
+.no-color {
+    background-color: rgb(255,255,255,0);
+}
+
+.no-color:hover {
+    background-color: rgba(255, 255, 255, .25) !important;
+}
+
+.center-center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.image-button-text {
+    font-size: 2rem;
+    color: #fff;
+}
+
+.image-button:hover {
+    
+    background-color: rgb(223, 222, 222);
+    
+    color: inherit;
+    cursor: pointer;
+    
+}
+
+@media only screen and (min-width: 960px) {
+    .image-button, .image-button-container {
+        /* width: 100%;
+        height: 100%; */
+        width: 300px;
+        height: 300px;
+    }
+
+    .image-button-container {
+        margin: 10px;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <div class="welcome-text">
+            <h1>Welcome to Sowards' Suites</h1>
+            <h4>What can we help you with?</h4>
+        </div>
+        <div name="home-buttons" class="home-buttons">
+            <ImageButton to="/rooms" name="Rooms" img="room2.jpg"/>
+            <ImageButton to="/book" name="Book"/>
+            <ImageButton to="/about" name="About"/>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ImageButton from '@/components/ImageButton.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+      ImageButton
   }
 }
 </script>
+
+<style scoped>
+.welcome-text h1, h2, h3, h4, h5, h6 {
+    margin: 20px;
+}
+
+.home-buttons {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+@media only screen and (min-width: 960px) {
+    .home-buttons {
+            flex-direction: row;
+    }
+}
+</style>
