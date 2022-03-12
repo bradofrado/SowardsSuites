8335f0fcffaf1d46d4aa9f2d0c4482be25a25905
Braydon Jones <bradofrado@gmail.com>
2022-03-12 15:56:45 -0700

    Updated about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about-page">
+        <h1>About</h1>
+        <p>Sowards' Suites is a website that lets <strong>you</strong> be in control of where you will be staying
+            during the next Sowards visit. With the Lemberholes' recent move in, it is more important than ever to 
+            reserve your favorite, limited offer room.</p>
+        <p>Navigate over to the Rooms page to see all of the possible rooms. (Yes even the Lemperholes' room can be reserved.
+            Shh, don't tell them!) Upon clicking on the room you will find information about the room and lovely pictures.
+            You can see the availablity and book the room their, or you can navigate over to the Book tab and see what is available
+            based on a specific date.</p>
+        <p>We hope you will enjoy your stay here at the Sowards' Suite. We know Mamma Llamma will for sure.</p>
+    </div>
 </template>
 
 <script>
@@ -12,3 +20,15 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.about-page h1, p {
+    margin-bottom: 2rem;
+}
+
+@media only screen and (min-width: 960px) {
+    .about-page {
+        margin: auto 250px;
+    }
+}
+</style>
