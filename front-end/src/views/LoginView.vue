27fe219ef007a384cecb191d6326321e15f895bb
Braydon Jones <bradofrado@gmail.com>
2022-03-18 00:37:14 -0600

    Ability to create a user

--- front-end/src/views/LoginView.vue
+++ front-end/src/views/LoginView.vue
@@ -1,20 +1,53 @@
 <template>
-    <div class="login-page">
+<div class="login-page">
+    <div v-if="user">
+        <h1>Welcome, {{user.firstname}}!</h1>
+        <a href="" @click.prevent="logout">Log out</a>
+    </div>
+    <div v-else-if="loggingIn">
         <h1>Log in</h1>
         <p>Welcome to Sowards' Suites</p>
         <div class="signin-container">
             <form class="d-flex flex-column">
                 <div class="form-field">
-                    <input type='text' placeholder="Email" v-model="username"/>
+                    <input type='text' placeholder="Username" v-model="username"/>
                 </div>
                 <div class="form-field">
                     <input type='password' placeholder="Password" v-model="password"/>
                 </div>
                 <button type="submit" class="button button-primary" @click.prevent="login">Log in</button>
                 <p class="error-text" v-if="error">{{error}}</p>
+                <a href="" @click.prevent="toggleLoginView">Sign up</a>
             </form>
         </div>
     </div>
+    <div v-else>
+        <h1>Sign Up</h1>
+        <div class="signin-container">
+            <form class="d-flex flex-column">
+                <fieldset>
+                    <div class="form-field">
+                        <input type='text' placeholder="First Name" v-model="firstname"/>
+                    </div>
+                    <div class="form-field">
+                        <input type='text' placeholder="Last Name" v-model="lastname"/>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div class="form-field">
+                        <input type='text' placeholder="Username" v-model="username"/>
+                    </div>
+                    <div class="form-field">
+                        <input type='password' placeholder="Password" v-model="password"/>
+                    </div>
+                </fieldset>
+                <button type="submit" class="button button-primary" @click.prevent="login">Sign Up</button>
+                <p class="error-text" v-if="error">{{error}}</p>
+                <a href="" @click.prevent="toggleLoginView">Log in</a>
+            </form>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -26,13 +59,33 @@ export default {
         return {
             password: "",
             username: "",
-            error: null
+            error: null,
+            loggingIn: true
         }
     },
-    mounted() {
-        this.$root.$data.isLoading = false;
+    async created() {
+        try {
+            let response = await axios.get('/api/users');
+            this.$root.$data.user = response.data.user;
+            this.$root.$data.isLoading = false;
+        } catch(error) {
+            this.$root.$data.user = null;
+            this.$root.$data.isLoading = false;
+        }
+    },
+    computed: {
+        user() {
+            return this.$root.$data.user;
+        }
     },
     methods: {
+        clearAll() {
+            this.username = "";
+            this.password = "";
+            this.firstname = "";
+            this.lastname = "";
+            this.error = "";
+        },
         async login() {
             try {
                 this.$root.$data.isLoading = true;
@@ -41,16 +94,50 @@ export default {
                     password: this.password
                 });
                 
-                this.$root.$data.user = response.data;
+                this.$root.$data.user = response.data.user;
                 this.$root.$data.isLoading = false;
-                this.username = "";
-                this.password = "";
+                
+                this.clearAll();
             } catch(error) {
                 console.log(error);
                 this.$root.$data.user = null;
                 this.$root.$data.isLoading = false;
                 this.error = error.response.data.message;
             }
+        },
+        async register() {
+            try {
+                this.$root.$data.isLoading = true;
+                const response = await axios.post('/api/users', {
+                    username: this.username,
+                    password: this.password,
+                    firstname: this.firstname,
+                    lastname: this.lastname
+                });
+                this.$root.$data.user = response.data.user;
+                this.$root.$data.isLoading = false;
+
+                this.clearAll();
+            } catch(error) {
+                console.log(error);
+                this.$root.$data.user = null;
+                this.$root.$data.isLoading = false;
+                this.error = error.response.data.message;
+            }
+        },
+        async logout() {
+            try {
+                this.$root.$data.isLoading = true;
+                await axios.delete('api/users');
+                this.$root.$data.user = null;
+            } catch(error) {
+                console.log(error);
+            }
+            this.$root.$data.isLoading = false;
+        },
+        toggleLoginView() {
+            this.loggingIn = !this.loggingIn;
+            this.clearAll();
         }
     }
 }
@@ -72,6 +159,10 @@ export default {
     margin: 0 auto;
     width: 300px;
     padding: 20px;
+}
+
+.login-page fieldset {
+    margin:10px;
 }
 
 .form-field {
